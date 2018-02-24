71aead9119556cde1b69e835f9ccbee1800e558e
Aditya Parakh <aparakh@hawk.iit.edu>
2018-02-24 02:34:07 -0600

    Added time stuff

--- firebase/authentication.py
+++ firebase/authentication.py
@@ -1,16 +1,20 @@
 from firebase import firebase
 class Firebase:
-    DATABASE_URL = "https://hackilln-2e7ee.firebaseio.com/"
+    DATABASE_URL = "https://hackill-b8ec1.firebaseio.com/"
 
     def __init__(self):
-        self.furr = firebase.FirebaseApplication(self.DATABASE_URL, None)
+        self.firebase = firebase.FirebaseApplication(self.DATABASE_URL, None)
 
     def getUsers(self):
-        return self.furr.get('/users', None)
+        return self.firebase.get('/users', None)
 
     def setUsers(self):
-        result = self.furr.post('/strings', data={"whatever":"data"}, params={'print': 'pretty'})
+        result = self.firebase.post('/2018/02/24/00/40', data={"fucku":"hamza"}, params={'print': 'pretty'})
         if result != None:
             return True
         else:
             return False
+
+# test
+boop = Firebase() # initialize the firebase
+boop.setUsers() 
